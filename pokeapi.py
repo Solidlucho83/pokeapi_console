ed2173c2510ceff3496ede12bb562e54e72fe4cf
Luis Castañeira <52086707+Solidlucho83@users.noreply.github.com>
2023-02-25 06:06:30 -0300

    Clean code

--- pokeapi.py
+++ pokeapi.py
@@ -7,48 +7,66 @@ from termcolor import colored
 
 # URL de la API de Pokémon
 URL = "https://pokeapi.co/api/v2/pokemon/"
+WELCOME_MSG = "Ingresa el Nombre del Pokémon (o 'salir' para finalizar) => "
+EXIT = "salir"
 
-def get_pokemon(name):
-    # Hacer una solicitud GET a la API de Pokémon para obtener la información del Pokémon
+COLOR_GREEN = "green"
+COLOR_BLUE = "blue"
+COLOR_MAGENTA = "magenta"
+COLOR_RED = "red"
+COLOR_YELLOW = "yellow"
+COLOR_CYAN = "cyan"
+COLOR_GREY = "grey"
+
+def get_pokemon_data(name):
     response = requests.get(URL + name.lower())
     response.raise_for_status()
-    pokemon_data = response.json()
-    sprite_url = response.json()["sprites"]["front_default"]
+    return response.json()
+
+def prepare_pokemon_image(pokemon_data):
+    sprite_url = pokemon_data["sprites"]["front_default"]
     response = requests.get(sprite_url)
     with open(f"pokemon.png", "wb") as f:
         f.write(response.content)
 
     os.chmod(f"pokemon.png", 0o755)
 
-    time.sleep(0.5)
-  
+def print_pokemon_data(pokemon_data):
+    print(colored("Nombre:", COLOR_GREEN), pokemon_data["name"])
+    print(colored("Peso:", COLOR_BLUE), f"{pokemon_data['weight']} kg")
+    print(colored("Altura:", COLOR_MAGENTA), f"{pokemon_data['height']} m")
+    print(colored("HP:", COLOR_RED), pokemon_data["stats"][0]["base_stat"])
+    print(colored("Ataque:", COLOR_YELLOW), pokemon_data["stats"][1]["base_stat"])
+    print(colored("Defensa:", COLOR_CYAN), pokemon_data["stats"][2]["base_stat"])
+    print(colored("Tipo:", COLOR_GREY), pokemon_data["types"][0]["type"]["name"])
+
+def show_pokemon(pokemon_data):
     try:
         with Image.open(f"pokemon.png") as img:
-          
             ascii_art = ascii_magic.from_image("pokemon.png")
-       
             print()
             ascii_art.to_terminal(columns=120, width_ratio=3)
-            print(colored("Nombre:", "green"), pokemon_data["name"])
-            print(colored("Peso:", "blue"), f"{pokemon_data['weight']} kg")
-            print(colored("Altura:", "magenta"), f"{pokemon_data['height']} m")
-            print(colored("HP:", "red"), pokemon_data["stats"][0]["base_stat"])
-            print(colored("Ataque:", "yellow"), pokemon_data["stats"][1]["base_stat"])
-            print(colored("Defensa:", "cyan"), pokemon_data["stats"][2]["base_stat"])
-            print(colored("Tipo:", "grey"), pokemon_data["types"][0]["type"]["name"])
+            print_pokemon_data(pokemon_data)
             print()
 
     except Exception as e:
         print(f"Error al cargar la imagen: {e}")
 
+def check_pokemon_found(response, pokemon_name):
+    if response.status_code != 200:
+        print(colored(f"No se encontró ningún Pokémon con el nombre {pokemon_name}", "red"))
+        return False
+    return True
+
 if __name__ == "__main__":
-      while True:
-        pokemon_name = input(colored("Ingresa el Nombre del Pokémon (o 'salir' para finalizar) => ", "blue")).lower()
-        if pokemon_name == "salir":
+    while True:
+        pokemon_name = input(colored(WELCOME_MSG, COLOR_BLUE)).lower()
+        if pokemon_name == EXIT:
             break
         response = requests.get(URL + pokemon_name)
-        if response.status_code != 200:
-            print(colored(f"No se encontró ningún Pokémon con el nombre {pokemon_name}", "red"))
-        else: 
+        if check_pokemon_found(response, pokemon_name):
             print()
-            get_pokemon(pokemon_name)
+            pokemon_data = get_pokemon_data(pokemon_name)
+            prepare_pokemon_image(pokemon_data)
+            show_pokemon(pokemon_data)
+
